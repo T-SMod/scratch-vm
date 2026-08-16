460f1e2e431c85e298b7ba719e1cd84fcf7b5c48
TSMod <Tagir-Studio@yandex.ru>
2026-08-16 11:12:38 +0300

    Dash => TSMod

--- src/engine/tw-platform.js
+++ src/engine/tw-platform.js
@@ -2,6 +2,6 @@
 // This can be accessed externally on `vm.runtime.platform`
 
 module.exports = {
-    name: 'Dash',
-    url: 'https://dashblocks.org/'
+    name: 'TSMod',
+    url: 'https://t-smod.github.io/'
 };
